127fed60dad5ad1927c12bc81621edb9da953be2
KamillaSV <kamilo4ka.92@mail.ru>
2022-05-09 18:12:24 +0500

    feat: settings webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { resolve } = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const { config } = require('process');
+
+const optimization = () => {
+    if (isProd) {
+        config.minimizer = [
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+}}
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 
 module.exports = {
     mode: 'development',
@@ -12,7 +27,10 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CopyWebpackPlugin({
             patterns: [
@@ -20,13 +38,16 @@ module.exports = {
                 from: path.resolve(__dirname, './src/favicon.ico'),
                 to: path.resolve(__dirname, 'dist')
             }
-        ]})
+        ]}),
+        new MiniCssExtractPlugin(),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: 
+                [isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                'css-loader']
             },
             {
                 test: /\.js$/,
@@ -37,5 +58,6 @@ module.exports = {
                 use: ["file-loader"]
             }
         ]
-    }
+    },
+    optimization: optimization ()
 };
